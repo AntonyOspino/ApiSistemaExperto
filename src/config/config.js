6ecc21c2d7bb4649f28d0c26feca2fac7875bfab
Alejo <alejandrocantillop.ilusa@gmail.com>
2025-09-03 22:20:24 -0500

    Que no joda mas clever por favor 2.0

--- src/config/config.js
+++ src/config/config.js
@@ -1,16 +1,24 @@
-const mysql = require("mysql2/promise")
-require('dotenv').config(
-    { path: ['.env.local', '.env']}
-);
-const db = mysql.createPool({
-    host: process.env.HOST || "localhost",
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 50,
-    port: process.env.PORT || 3306
-})
+// En tu archivo de configuración de Express (db.js)
+const mysql = require("mysql2/promise");
+require('dotenv').config({ path: ['.env.local', '.env'] });
 
-module.exports = db
+// Usa el objeto global para almacenar la única instancia del pool
+if (global.dbPool) {
+    console.log("Usando la instancia de pool existente.");
+    module.exports = global.dbPool;
+} else {
+    console.log("Creando una nueva instancia de pool.");
+    const db = mysql.createPool({
+        host: process.env.HOST || "localhost",
+        user: process.env.USER,
+        password: process.env.PASSWORD,
+        database: process.env.DATABASE,
+        waitForConnections: true,
+        connectionLimit: 10,
+        queueLimit: 50,
+        port: process.env.PORT || 3306
+    });
+
+    global.dbPool = db;
+    module.exports = db;
+}
